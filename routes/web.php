3ffde07c1cbf3b2e8ec71c8e70cec15f71843b1d
nguyenquochuy2k1 <dh51905009@student.stu.edu.vn>
2023-02-28 20:23:46 +0700

    update package.json

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', [ProductController::class, 'home'])->name('admin.home');
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/', [ProductController::class, 'home'])->name('admin.home');
 // Route::get('/admin', function () {
 //     return view('admin');
 // })->middleware(['auth', 'verified'])->name('admin');
